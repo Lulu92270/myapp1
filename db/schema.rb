8778570f23f101f8037c33f47ef890a87e076764
lucien andrieux <lucien.andrieux@epfedu.fr>
2020-12-28 21:04:02 +0000

    switching database from sqlite3 to PstgreSQL

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_12_21_220327) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "flats", force: :cascade do |t|
     t.string "name", null: false
     t.string "image_url"
