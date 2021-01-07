f42752b2cd25cab0eaff21812811c1cdb16e971b
lucien andrieux <lucien.andrieux@epfedu.fr>
2021-01-07 11:50:31 +0000

    click me periodic display when a card is selected

--- app/javascript/flat/components/Flat.jsx
+++ app/javascript/flat/components/Flat.jsx
@@ -5,29 +5,25 @@ import './styles/Flat.scss';
 const Flat = ({imgUrl, title, onSelect, id, selected}) => {
   useEffect(() => {
     const interval = setInterval(() => {
-      setSeconds(seconds => seconds + 1);
+      boolean = !boolean;
+      setSecond(boolean ? 'Click me!' : title);
     }, 1000);
     return () => clearInterval(interval);
   }, []);
 
-  const [seconds, setSeconds] = useState(0);
-  const description = selected ? 'Click me!' : title;
+  let boolean = true;
+  const [second, setSecond] = useState(title);
   const classes = selected ? ' selected' : '';
   const showPath = "/flats/" + id;
   const history = useHistory();
 
-  const handleClick = () => {
-    return seconds;
-    // console.log(seconds);
-  }
-
   return (
-    <div className={'flat rounded' + classes} onClick={handleClick}> 
+    <div className={'flat rounded' + classes} > 
       <div onClick={() => onSelect(id)} >
         <img src={imgUrl} className="flat-image" alt="Flat" />
       </div>
       <div className="flat-title" onClick={() => history.push(showPath)}>
-        {title}
+        {selected ? second : title}
       </div>
     </div>
   );
